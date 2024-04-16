2c68596c10df784fb82a320c7bfc9ed58f523623
Bart <bart.rybak@hotmail.com>
2024-04-16 23:49:39 +0100

    Add project files.

---
+++ Controllers/GatesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HumanDesign.Data;
+using HumanDesign.Models;
+
+namespace HumanDesign.Controllers
+{
+    public class GatesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GatesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Gates
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Gates.ToListAsync());
+        }
+
+        // GET: Gates/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gate = await _context.Gates
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (gate == null)
+            {
+                return NotFound();
+            }
+
+            return View(gate);
+        }
+
+        // GET: Gates/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Gates/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description")] Gate gate)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(gate);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(gate);
+        }
+
+        // GET: Gates/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gate = await _context.Gates.FindAsync(id);
+            if (gate == null)
+            {
+                return NotFound();
+            }
+            return View(gate);
+        }
+
+        // POST: Gates/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] Gate gate)
+        {
+            if (id != gate.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(gate);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GateExists(gate.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(gate);
+        }
+
+        // GET: Gates/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gate = await _context.Gates
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (gate == null)
+            {
+                return NotFound();
+            }
+
+            return View(gate);
+        }
+
+        // POST: Gates/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var gate = await _context.Gates.FindAsync(id);
+            if (gate != null)
+            {
+                _context.Gates.Remove(gate);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GateExists(int id)
+        {
+            return _context.Gates.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using HumanDesign.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HumanDesign.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Gate> Gates { get; set; }
+    }
+}
